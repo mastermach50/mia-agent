71636fb534d593b19cbb6ce55804352da84fc583
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2026-06-02 16:38:53 +0530

    Add enviroments parsing and logging

--- src/config.rs
+++ src/config.rs
@@ -3,6 +3,7 @@ use std::sync::OnceLock;
 
 use anyhow::{Context, Result};
 use config::{Config, Environment, File};
+use log::{debug, info};
 use serde::{Deserialize, Serialize};
 
 /// Cached config that is loaded on load() and accessed on global()
@@ -28,6 +29,7 @@ impl Default for AppConfig {
 pub struct ModelConfig {
     pub name: String,
     pub provider: String,
+    pub api_key: Option<String>,
 }
 
 impl Default for ModelConfig {
@@ -35,6 +37,7 @@ impl Default for ModelConfig {
         Self {
             name: "owl-alpha".to_string(),
             provider: "openrouter".to_string(),
+            api_key: None,
         }
     }
 }
@@ -59,9 +62,12 @@ impl Default for DocumentConfig {
 impl AppConfig {
     /// Fetches cached global config.
     pub fn global() -> &'static AppConfig {
-        CONFIG_CACHE
+        let cached_config = CONFIG_CACHE
             .get()
-            .expect("Failed to load cached config")
+            .expect("Failed to load cached config");
+
+        debug!("Fetched config from cache");
+        cached_config
     }
 
     /// Loads config to the cache and returns it.
@@ -69,15 +75,34 @@ impl AppConfig {
     pub fn load() -> Result<Self> {
         let home_dir = std::env::home_dir().unwrap();
         let mia_dir = home_dir.join(".mia");
+        let env_file = mia_dir.join(".env");
         let config_file = mia_dir.join("config.toml");
 
-        // Create config file if it doesn't exist
-        if !mia_dir.exists() || !config_file.exists() {
+        // Create home dir, mia dir, config.toml and .env if they don't exist
+        if !home_dir.exists() {
+            fs::create_dir_all(&home_dir)
+                .context("Failed to create home dir")?;
+            info!("Created home directory at {:?}", home_dir);
+        }
+        if !mia_dir.exists() {
             fs::create_dir_all(&mia_dir)
                 .context("Failed to create agent home dir")?;
+            info!("Created agent home directory at {:?}", mia_dir);
+        }
+        if !env_file.exists() {
+            fs::write(&env_file, "")?;
+            info!("Created default .env file at {:?}", env_file);
+        }
+        if !config_file.exists() {
             fs::write(&config_file, toml::to_string(&AppConfig::default())?)
                 .context("Failed to create default config file")?;
+            info!("Created default config file at {:?}", config_file);
         }
+
+        // Load .env to environment variables
+        dotenvy::from_path(&env_file)
+            .context("Failed to load .env file")?;
+        debug!("Loaded .env file from {:?}", env_file);
 
         // Build config with priority
         // Env > Config file
@@ -87,7 +112,7 @@ impl AppConfig {
             )
             .add_source(
                 Environment::with_prefix("MIA")
-                    .separator("_")
+                    .separator("__")
             )
             .build()
             .context("Failed to assemble configuration sources");
@@ -96,6 +121,7 @@ impl AppConfig {
             .try_deserialize()?;
         
         let _ = CONFIG_CACHE.set(app_config.clone());
+        debug!("Loaded and cached config");
 
         Ok(app_config)
     }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
 use anyhow::{Ok, Result};
 use clap::Parser;
+use env_logger::Env;
+use std::env;
 
 mod config;
 mod tui;
 mod cli;
 use cli::Cli;
 use config::AppConfig;
+use log::{info, trace};
 
 fn main() -> Result<()>{
+
+    // Setup logging
+    let env = Env::new().filter_or("MIA_LOG", "info");
+    env_logger::init_from_env(env);
+
+    // Parse CLI args
     let cli = Cli::parse();
     
+    // Load configs from config.toml (to global cache)
     AppConfig::load()?;
+
+    trace!("Model: {:?}", AppConfig::global().model);
+    trace!("Documents: {:?}", AppConfig::global().documents);
     
     match cli.command {
         Some(cli::Commands::Gateway) => {
-            println!("Starting gateway...");
+            info!("Starting gateway...");
             // TODO start gateway server
         },
         Some(cli::Commands::Tui) => {
-            println!("Starting TUI...");
+            info!("Starting TUI...");
             tui::run()?;
         },
         None => {
